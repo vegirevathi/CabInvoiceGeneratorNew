0187608779c8bc29f898f81685a8b44a23d766b7
Amrut <amrutpanda7789@gmail.com>
2020-07-10 19:36:29 +0530

    ADDED: Given less distance and time should return minimum fare

--- src/test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
@@ -6,12 +6,21 @@ import org.junit.Test;
 public class CabInvoiceGeneratorTest {
 
     @Test
-    public void givenInvoiceGeneratorShouldReturnTotalFare() {
+    public void givenDistanceAndTime_ShouldReturnTotalFare() {
         CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
         double distance = 3.0;
         int time = 10;
         double totalFare = cabInvoiceGenerator.totalFare(distance, time);
         Assert.assertEquals(40,totalFare,0.0);
 
+    }
+
+    @Test
+    public void givenLessDistanceAndTime_ShouldReturnMinFare() {
+        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double totalFare = cabInvoiceGenerator.totalFare(distance, time);
+        Assert.assertEquals(5,totalFare,0.0);
     }
 }
