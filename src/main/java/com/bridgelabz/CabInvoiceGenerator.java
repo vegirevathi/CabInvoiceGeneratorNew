819ddaaaefb2d0b65a763d94507be96a1562fcdc
Amrut <amrutpanda7789@gmail.com>
2020-07-10 19:03:18 +0530

    ADDED: Calculate total fare for cab

--- src/main/java/com/bridgelabz/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/CabInvoiceGenerator.java
@@ -2,7 +2,11 @@ package com.bridgelabz;
 
 public class CabInvoiceGenerator {
 
+    private static final int COST_PER_TIME = 1;
+    private static final double MINIMUM_COST_PER_KM = 10;
+
     public double totalFare(double distance, int time) {
-        return 0;
+
+        return distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
     }
 }
