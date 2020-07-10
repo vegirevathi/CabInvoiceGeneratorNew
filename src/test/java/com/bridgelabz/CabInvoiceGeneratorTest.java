aa8d529af504809b3a77eb3555f8f25005933047
vegirevathi <vegirevathi@gmail.com>
2020-07-10 17:04:30 +0530

    ADDED: Test method to calculate total fare

--- src/test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class CabInvoiceGeneratorTest {
+
     @Test
     public void givenInvoiceGeneratorShouldReturnTotalFare() {
         CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
