26230b5571d56816dd89cb72cdccb9b61f1e482c
Amrut <amrutpanda7789@gmail.com>
2020-07-10 20:03:27 +0530

    ADDED: Calculate total fare for multiple rides

--- src/main/java/com/bridgelabz/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/CabInvoiceGenerator.java
@@ -6,13 +6,27 @@ public class CabInvoiceGenerator {
     private static final double MINIMUM_COST_PER_KM = 10;
     private static final double MINIMUM_FARE = 5 ;
 
+    /**
+     *
+     * @param distance
+     * @param time
+     * @return
+     */
     public double totalFare(double distance, int time) {
-
         double totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
         return Math.max(totalFare, MINIMUM_FARE);
     }
 
+    /**
+     *
+     * @param rides
+     * @return
+     */
     public double totalFare(Rides[] rides) {
-        return 0;
+        double totalFare =0;
+        for (Rides ride : rides) {
+            totalFare += this.totalFare(ride.distance, ride.time);
+        }
+        return totalFare;
     }
 }
--- src/main/java/com/bridgelabz/Rides.java
+++ src/main/java/com/bridgelabz/Rides.java
@@ -1,6 +1,11 @@
 package com.bridgelabz;
 
 public class Rides {
+    public final double distance;
+    public final int time;
+
     public Rides(double distance, int time) {
+        this.distance = distance;
+        this.time = time;
     }
 }
