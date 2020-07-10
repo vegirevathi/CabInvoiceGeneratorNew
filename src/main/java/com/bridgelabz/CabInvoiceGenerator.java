eba90678103ec77ed48897f4f76119814681df5f
vegirevathi <vegirevathi@gmail.com>
2020-07-10 19:41:51 +0530

    ADDED: Functionality to return minimum fare

--- src/main/java/com/bridgelabz/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/CabInvoiceGenerator.java
@@ -4,9 +4,11 @@ public class CabInvoiceGenerator {
 
     private static final int COST_PER_TIME = 1;
     private static final double MINIMUM_COST_PER_KM = 10;
+    private static final double MINIMUM_FARE = 5 ;
 
     public double totalFare(double distance, int time) {
 
-        return distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
+        double totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
+        return Math.max(totalFare, MINIMUM_FARE);
     }
 }
